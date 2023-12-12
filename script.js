89602caabd23161eac4a2847636e8511e9d1d65e
NoorTushar <tushar.noor.khan@gmail.com>
2023-12-12 22:38:53 +0600

    initialize

---
+++ script.js
@@ -0,0 +1,74 @@
+/*
+Falsy Values: false, 0, -0, 0n(BigInt Value), ""(empty string), null, undefined, NaN
+
+Truthy Values: Everything Else
+
+*/
+
+if (true) {
+  console.log(`this is a truthy value`);
+} else {
+  console.log(`this is a falsy value`);
+}
+//returns: `this is a truthy value`
+
+if (false) {
+  console.log(`this is a truthy value`);
+} else {
+  console.log(`this is a falsy value`);
+}
+//returns: `this is a falsy value`
+
+0
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a falsy value`
+
+1
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a truthy value`
+
+"Noor"
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a truthy value`
+
+-0
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a falsy value`
+
+""
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a falsy value`
+
+null
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a falsy value`
+
+undefined
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a falsy value`
+
+NaN
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a falsy value`
+
+0n
+  ? console.log(`this is a truthy value`)
+  : console.log(`this is a falsy value`);
+
+//returns: `this is a falsy value`
